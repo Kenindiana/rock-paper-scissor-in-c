f5a7e57063f9873cc33cf2d7ce04cb450a3982e9
Kenindiana <116534468+Kenindiana@users.noreply.github.com>
2022-10-24 22:15:26 +0700

    Add files via upload

---
+++ gunting batu kertas.c
@@ -0,0 +1,431 @@
+#include<stdio.h>
+//dibuat oleh Ken Indiana Arizona dengan Dev c++
+//permainan gunting batu kertas sederhana
+int main()
+{
+//	declare
+	int poin,x,y,a,b;
+	char nama[1001];
+//	menscan nama
+	printf("masukan nama anda: ");
+	gets(nama);
+//	membuka file
+	FILE *game=fopen("save.txt","r");
+	printf("cat: ketik angka untuk memilih\n\n");
+//	delay
+	for(x=1;x<=20000;x++)
+	{
+		for(y=1;y<=20000;y++)
+		{
+		}
+	}
+	poin=0;
+//	looping infinity yang hanya akan keluar bila keadaan yang menghasilkan goto terjadi
+	while(poin >= 0)
+	{
+//		kata pembuka dengan dianimasi seakan akan mengetik dengan looping
+		printf("s");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("i");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+		printf("l");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("a");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("h");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("k");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }printf("a");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("n");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }printf(" ");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=10000;y++)
+		    {
+		    }
+	    }
+	    printf("k");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("e");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("t");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    
+	    printf("i");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("k");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf(" ");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("a");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("n");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("g");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("k");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("a");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    
+	    printf(" ");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("1");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("(");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+		printf("g");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("u");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("n");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("t");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("i");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("n");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	     printf("g");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf(")");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("/");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("2");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("(");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+		printf("b");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("a");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("t");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	     printf("u");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf(")");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+		printf("/");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("3");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("(");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+		printf("k");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("e");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("r");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	     printf("t");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("a");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf("s");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+	    printf(")\n=");
+		for(x=1;x<=5000;x++)
+	    {
+		    for(y=1;y<=5000;y++)
+		    {
+		    }
+	    }
+		masuk:
+		scanf("%d",&a);
+		if(a<1,a>3)
+		{
+			printf("input salah mohon masukan yang benar\n=");
+			goto masuk;
+		}
+		b=rand()%3+1;;
+		if(b == 1)
+		{
+			printf("npc => gunting\n");
+		}else if(b == 2)
+		{
+			printf("npc => batu\n");
+		}else
+		{
+			printf("npc => kertas\n");
+		}
+		if(a == b)
+		{
+			printf("seri\n\n");
+		}else if(a == 1 && b == 3)
+		{
+			printf("selamat anda menang\n\n");
+			poin++;
+		}else if(a == 2 && b ==1)
+		{
+			printf("selamat anda menang\n\n");
+			poin++;
+		}else if(a == 3 && b == 2)
+		{
+			printf("selamat anda menang\n\n");
+			poin++;
+		}else
+		{
+			printf("anda kalah\n\n");
+			goto end;
+		}
+	}
+	end:
+	printf("%s: %d\n",nama,poin);
+	for(x=1;x<=30000;x++)
+	    {
+		    for(y=1;y<=30000;y++)
+		    {
+		    }
+	    }
+	int tes;
+	char nickname;
+	fscanf(game,"%d",&tes);
+	printf("last highscore: %d\n",tes);
+	if(poin>tes)
+	{
+		printf("selamat anda mencapai highscore\n");
+		fclose(game);
+		game=fopen("save.txt","w");
+		fprintf(game,"%d",poin);
+	}
+	
+	return 0;
+	
+}
